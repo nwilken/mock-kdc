d2d3184aedd620f875bef6a2337c5ab25bfed623
Nate Wilken <wilken@asu.edu>
2020-05-31 11:25:17 -0700

    abstract KRB5_REALM; build db in entrypoint

--- Dockerfile
+++ Dockerfile
@@ -11,12 +11,6 @@ RUN mkdir /docker-entrypoint-init.d
 COPY docker-entrypoint.sh /usr/local/bin/
 ENTRYPOINT ["docker-entrypoint.sh"]
 
-COPY krb5.conf /etc/
-COPY kdc.conf /var/kerberos/krb5kdc/
-COPY kadm5.acl /var/kerberos/krb5kdc/
-
-RUN kdb5_util -r ASU.EDU -P password create -s
-
 EXPOSE 88
 
 CMD ["/usr/sbin/krb5kdc", "-n"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,50 @@
 set -eo pipefail
 shopt -s nullglob
 
+[ -z ${KRB5_REALM} ] && echo "missing KRB5_REALM" && exit 1
+
+cat <<EOF > /etc/krb5.conf
+[libdefaults]
+    default_realm = ${KRB5_REALM}
+    dns_lookup_realm = false
+    dns_lookup_kdc = false
+    ticket_lifetime = 24h
+    renew_lifetime = 7d
+    rdns = false
+
+[realms]
+    ${KRB5_REALM} = {
+        kdc = localhost
+        admin_server = localhost
+    }
+EOF
+
+cat <<EOF > /var/kerberos/krb5kdc/kdc.conf
+default_realm = ${KRB5_REALM}
+
+[logging]
+    default = CONSOLE
+
+[kdcdefaults]
+    kdc_ports = 88
+    kdc_tcp_ports = 88
+
+[realms]
+    ${KRB5_REALM} = {
+        master_key_type = aes256-cts
+        acl_file = /var/kerberos/krb5kdc/kadm5.acl
+        dict_file = /usr/share/dict/words
+        admin_keytab = /var/kerberos/krb5kdc/kadm5.keytab
+        key_stash_file = /var/kerberos/krb5kdc/stash
+        supported_enctypes = aes256-cts:normal aes128-cts:normal
+        default_principal_flags = +preauth
+    }
+EOF
+
+cat <<EOF > /var/kerberos/krb5kdc/kadm5.acl
+*/admin@${KRB5_REALM}	*
+EOF
+
 process_init_files() {
     local f
     for f; do
@@ -24,14 +68,16 @@ process_init_files() {
     done
 }
 
-# if command starts with an option, prepend mysqld
-if [ "${1:0:1}" = '-' ]; then
-    set -- krb5kdc "$@"
-fi
+kdb5_util -r ${KRB5_REALM} -P password create -s
 
 # check dir permissions
 ls /docker-entrypoint-init.d/ > /dev/null
 
 process_init_files /docker-entrypoint-init.d/*
+
+# if command starts with an option, prepend mysqld
+if [ "${1:0:1}" = '-' ]; then
+    set -- krb5kdc "$@"
+fi
 
 exec "$@"
